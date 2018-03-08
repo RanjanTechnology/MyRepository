1958ff06234779af37c3a2e290a57f0df21232bc
Singh,Priyaranjan <Priyaranjan.Singh@mastercard.com>
2018-03-08 15:04:55 +0530

    S0001 : My 1st commit project

---
+++ One-To-Many_Himernate/src/main/java/com/master/One_To_Many_Himernate/App.java
@@ -0,0 +1,13 @@
+package com.master.One_To_Many_Himernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
